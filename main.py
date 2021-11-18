e4b635b0e9e5287921c63abea3724f40bc8120d9
encoded <55350380+encodedQOL@users.noreply.github.com>
2021-11-18 15:45:31 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# project here!
+
+# blackjack in python 
+
+# by Reuben 
+
+import random 
+
+try: 
+
+    from cardList import renderHearts 
+
+except: 
+
+    f = open("cardList.py", "x") 
+
+
+def bankReset(): 
+
+    userChips = 100 
+
+    dealerChips = userChips 
+
+  
+
+     
+
+     
+
+renderHearts() 
